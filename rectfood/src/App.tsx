59839c79d52c7280af24feb1dc71beb6862fc773
Nickson Munene Njeru <nicknionene7@gmail.com>
2024-05-02 17:05:30 +0300

    next routing

--- rectfood/src/App.tsx
+++ rectfood/src/App.tsx
@@ -2,12 +2,22 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import '../app/globals.css'
 import Homepage from './pages/home-page'
+import RecipePage from './pages/recipe-pge'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/recipes/:recipId" element={<RecipePage />} />
+        <Route
+          path="*"
+          element={
+            <p className="text-red-500 text-center text-7xl font-bold mt-16">
+              Page not found
+            </p>
+          }
+        />
       </Routes>
     </>
   )
